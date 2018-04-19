414551512ce19d36034c14773a9012767f622ffd
Wayne Galen <wgalen@fastwaterheater.com>
2018-04-19 13:22:35 -0700

    Initial commit - Basic UI

---
+++ TaskSlayer/frmMain.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace TaskSlayer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listProcesses = new System.Windows.Forms.ListView();
+            this.btnKill = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listProcesses
+            // 
+            this.listProcesses.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listProcesses.Location = new System.Drawing.Point(12, 12);
+            this.listProcesses.Name = "listProcesses";
+            this.listProcesses.ShowGroups = false;
+            this.listProcesses.Size = new System.Drawing.Size(349, 538);
+            this.listProcesses.TabIndex = 0;
+            this.listProcesses.UseCompatibleStateImageBehavior = false;
+            this.listProcesses.View = System.Windows.Forms.View.Details;
+            // 
+            // btnKill
+            // 
+            this.btnKill.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnKill.Location = new System.Drawing.Point(250, 556);
+            this.btnKill.Name = "btnKill";
+            this.btnKill.Size = new System.Drawing.Size(111, 39);
+            this.btnKill.TabIndex = 1;
+            this.btnKill.Text = "Kill";
+            this.btnKill.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 607);
+            this.Controls.Add(this.btnKill);
+            this.Controls.Add(this.listProcesses);
+            this.Name = "frmMain";
+            this.Text = "TaskSlayer";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listProcesses;
+        private System.Windows.Forms.Button btnKill;
+    }
+}
+
